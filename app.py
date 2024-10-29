c0324cdb6e089553e11e60c87fcfbce0d24875c1
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-10-29 17:21:10 -0400

    Con todos los datos

---
+++ app.py
@@ -0,0 +1,204 @@
+import csv
+import math
+import random
+import seaborn
+import matplotlib
+from pprint import pprint
+
+lista = []
+
+def obtencion(lista, numero):
+    fa = lista['fa']
+    fi = lista['fi']
+    clase = lista['clase']
+    indice = 0
+    for esto in fa: 
+        if esto >= numero: 
+            indice = fa.index(esto)
+            break
+    if indice == 0: fa_numero = 0
+    else: fa_numero = fa[indice - 1]
+    fi_numero = fi[indice]
+    li = clase[indice]['minimo']
+    amplitud = clase[0]['maximo'] - clase[0]['minimo']
+    esto = li + ((numero - fa_numero) / fi_numero) * amplitud
+    return esto
+
+def mostrar_tabla(todo : dict): 
+    total = len(todo['clase'])
+    llaves = todo.keys()
+    texto = '|'
+    for esto in llaves: texto += f' {esto} |'
+    print(len(texto) * '-')
+    print(texto)
+    for i in range(total): 
+        texto = '|'
+        for esto in llaves: 
+            if esto == 'clase': 
+                minimo = todo[esto][i]['minimo']
+                maximo = todo[esto][i]['maximo']
+                texto += f' {minimo}-{maximo} |'
+            else: texto += f' {todo[esto][i]} |'
+        print(len(texto) * '-')
+        print(texto)
+    print(len(texto) * '-')
+
+def buscar_modales(fi : list): 
+    lista = []
+    maximo = max(fi)
+    for i in range(len(fi)): 
+        if fi[i] == maximo: 
+            lista.append(i)
+    return lista
+
+def creacion(r : list): 
+    minimo = min(r)
+    maximo = max(r)
+    n = len(r)
+    print(n)
+    rango = maximo - minimo 
+    k = 1 + 3.3 * math.log(n, 10)
+    amplitud = rango / k
+    print(f'amplitud: {amplitud}')
+    if amplitud > int(amplitud): 
+        # amplitud += 1
+        # amplitud = int(amplitud)
+        amplitud = round(amplitud)
+    print(f'amplitud: {amplitud}')
+    lista = {}
+    for esto in r: 
+        try: lista[esto] += 1
+        except: lista[esto] = 1
+    real = []
+    para = 0
+    while True: 
+        if minimo > maximo: break
+        para = minimo + amplitud - 1
+        clase = {
+            'minimo': int(minimo), 
+            'maximo': int(para + 1)
+        }
+        minimo = para + 1
+        real.append(clase)
+    oficial = [{'clase': real}]
+    oficial[0]['fi'] = []
+    for esto in real: 
+        numero = sum(map(lambda x: lista[x], filter(lambda x: x >= esto['minimo'] and x < esto['maximo'], lista.keys())))
+        oficial[0]['fi'].append(numero)
+    parte = []
+    for esto in oficial[0]['fi']: 
+        if len(parte) >= 1: parte.append(esto + parte[-1])
+        else: parte.append(esto)
+    oficial[0]['fa'] = parte
+    oficial[0]['xi'] = list(map(lambda x: (x['minimo'] + x['maximo']) / 2, oficial[0]['clase']))
+    oficial[0]['fi.xi'] = [(xi * fi) for xi, fi in zip(oficial[0]['fi'], oficial[0]['xi'])]
+    print(f'suma fixi {sum(oficial[0]["fi.xi"])}')
+    oficial[0]['fsr'] = list(map(lambda x: x / oficial[0]['fa'][-1], oficial[0]['fi']))
+    oficial[0]['far'] = list(map(lambda x: x / oficial[0]['fa'][-1], oficial[0]['fa']))
+    oficial[0]['fsr%'] = list(map(lambda x: x * 100, oficial[0]['fsr']))
+    oficial[0]['far%'] = list(map(lambda x: x * 100, oficial[0]['far']))
+    oficial[0]['fi.xi^2'] = [(xi * fixi) for xi, fixi in zip(oficial[0]['fi.xi'], oficial[0]['xi'])]
+    return oficial
+
+def proceso(todo : list): 
+    mostrar_tabla(todo[0])
+    total = todo[0]['fa'][-1]
+    # print(f'Total {total}')
+    a = todo[0]['clase'][0]['maximo'] - todo[0]['clase'][0]['minimo'] 
+    # print(f"Sum: {sum(todo[0]['fi.xi'])}")
+    print('Variables de posición: ')
+    cuartiles = []
+    for i in range(1, 5): 
+        q = obtencion(todo[0], ((25 * i) / 100) * total)
+        print(f'Q{i}: {round(q, 4)}')
+        cuartiles.append(q)
+    print('Variables de centralización: ')
+    media = sum(todo[0]['fi.xi']) / total
+    print(f'Media aritmética: {round(media, 4)}')
+    calculo = total / 2
+    fa = 0
+    for esto in todo[0]['fa']: 
+        if esto >= calculo: 
+            fa = esto
+            break
+    indice = todo[0]['fa'].index(fa)
+    li = todo[0]['clase'][indice]['minimo']
+    fi_menos = todo[0]['fa'][indice - 1]
+    fi = todo[0]['fi'][indice]
+    mediana = li + ((calculo - fi_menos) / fi) * a
+    print(f'Mediana: {round(mediana, 4)}')
+    lugares_modales = buscar_modales(todo[0]['fi'])
+    modales = []
+    for esto in lugares_modales: 
+        li = todo[0]['clase'][esto]['minimo']
+        fi = todo[0]['fi'][esto]
+        try: fi_menos = todo[0]['fi'][esto - 1]
+        except: fi_menos = 0
+        try: fi_mas = todo[0]['fi'][esto + 1]
+        except: fi_mas = 0
+        d1 = fi - fi_menos
+        d2 = fi - fi_mas
+        try: modal = li + (d1 / (d1 + d2)) * a
+        except: 
+            print(f'''"
+En la {lugares_modales.index(esto) + 1}° moda hubo una excepción que ameritó cambiar (d1 / (d1 + d2)) 
+porque daba una división de 0 sobre 0 que no está definida en matemática
+"''')
+            modal = li + a
+        modales.append(round(modal, 4))
+    print(f'Modales: {modales}')
+    print('Variables de variabilidad: ')
+    varianza = (sum(todo[0]['fi.xi^2']) / total) - (media**2)
+    print(f'Varianza: {round(varianza, 4)}')
+    desviacion = math.sqrt(varianza)
+    print(f'Desviación estándar: {round(desviacion, 4)}')
+    intercuartil = cuartiles[2] - cuartiles[0]
+    p75 = obtencion(todo[0], (75 / 100) * total)
+    p25 = obtencion(todo[0], (25 / 100) * total)
+    p90 = obtencion(todo[0], (90 / 100) * total)
+    p10 = obtencion(todo[0], (10 / 100) * total)
+    curtosis = ((p75 - p25) / (p90 - p10)) * 0.5
+    coeficiente = (desviacion / media) * 100
+    print(f'Coeficiente de variación: {round(coeficiente, 4)}')
+    print(f'Rango intercuartil: {round(intercuartil, 4)}')
+    print('variables de forma: ')
+    indice = (3 * (media - mediana)) / desviacion
+    if curtosis == 0: apuntamiento = 'Es mesocúrtica como la normal'
+    elif curtosis > 0: apuntamiento = 'Es leptocúrtica apuntada'
+    elif curtosis < 0: apuntamiento = 'Es platicúrtica aplanada'
+    print(f'Curtosis: {round(curtosis, 4)} ({apuntamiento})')
+    if indice == 0: simetria = 'Es simétrica'
+    elif indice > 0: simetria = 'Es asimétrica positiva con sesgo a la derecha'
+    elif indice < 0: simetria = 'Es asimétrica negativa con sesgo a la izquierda'
+    print(f'Índice de asimetría: {round(indice, 4)} ({simetria})')
+    print('++++++++++++++++++++++++++++++++++++++++++++')
+    print('**********************************************')
+    print('Clases: ')
+    pprint(todo[0]["clase"])
+    print('Frecuencias: ')
+    pprint(todo[0]["fi"])
+    print('**********************************************')
+
+def prueba(esto): 
+    try: 
+        float(esto)
+        return True
+    except: return False
+
+def primero(): 
+    with open('Datos proyecto 2024.csv', 'r') as archivo: 
+        lista = list(map(lambda x: dict(map(lambda y: (y[0], float(y[1])) if prueba(y[1]) else (y[0], y[1]), x.items())), csv.DictReader(archivo)))
+    grande = ['Age', 'Income', 'HoursWk']
+    for esto in grande: 
+        print('*' * 30)
+        print(f'[{esto}]')
+        print('*' * 30)
+        data = list(map(lambda x: x[esto], lista))
+        todo = creacion(data)
+        proceso(todo)
+
+def main(): 
+    primero()
+
+if __name__ == '__main__': 
+    main()
